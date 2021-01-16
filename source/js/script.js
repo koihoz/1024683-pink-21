8cd085dc1ac4a78a82844164128597fab5be9950
Alex <koihoz@mail.ru>
2021-01-16 16:04:45 +0100

    доработка скрипта

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,17 @@
 
-  var navMain = document.querySelector('.page-header');
-  var navToggle = document.querySelector('.page-header__toggle');
+  var pageHeader = document.querySelector('.page-header');
+  var pageHeaderToggle = document.querySelector('.page-header__toggle');
 
-  navMain.classList.remove('page-header--nojs');
+  pageHeader.classList.remove('page-header--nojs');
+  pageHeader.classList.add('page-header--closed');
+  pageHeader.classList.remove('page-header--opened');
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('page-header--closed')) {
-      navMain.classList.remove('page-header--closed');
-      navMain.classList.add('page-header--opened');
+  pageHeaderToggle.addEventListener('click', function() {
+    if (pageHeader.classList.contains('page-header--closed')) {
+      pageHeader.classList.remove('page-header--closed');
+      pageHeader.classList.add('page-header--opened');
     } else {
-      navMain.classList.add('page-header--closed');
-      navMain.classList.remove('page-header--opened');
+      pageHeader.classList.add('page-header--closed');
+      pageHeader.classList.remove('page-header--opened');
     }
   });
