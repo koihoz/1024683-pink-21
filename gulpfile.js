1c7e82cb0fc6c1a7abfdea41f66b18bc50d58498
Alex <koihoz@mail.ru>
2021-01-20 21:47:22 +0100

    исправления ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,9 @@ const styles = () => {
       autoprefixer(),
       csso()
     ]))
+    .pipe(rename("style.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
 
-  var pageHeader = document.querySelector('.page-header');
-  var pageHeaderToggle = document.querySelector('.page-header__toggle');
+  var pageHeader = document.querySelector(".page-header");
+  var pageHeaderToggle = document.querySelector(".page-header__toggle");
 
-  pageHeader.classList.remove('page-header--nojs');
-  pageHeader.classList.add('page-header--closed');
-  pageHeader.classList.remove('page-header--opened');
+  pageHeader.classList.remove("page-header--nojs");
+  pageHeader.classList.add("page-header--closed");
+  pageHeader.classList.remove("page-header--opened");
 
-  pageHeaderToggle.addEventListener('click', function() {
-    if (pageHeader.classList.contains('page-header--closed')) {
-      pageHeader.classList.remove('page-header--closed');
-      pageHeader.classList.add('page-header--opened');
+  pageHeaderToggle.addEventListener("click", function() {
+    if (pageHeader.classList.contains("page-header--closed")) {
+      pageHeader.classList.remove("page-header--closed");
+      pageHeader.classList.add("page-header--opened");
     } else {
-      pageHeader.classList.add('page-header--closed');
-      pageHeader.classList.remove('page-header--opened');
+      pageHeader.classList.add("page-header--closed");
+      pageHeader.classList.remove("page-header--opened");
     }
   });
